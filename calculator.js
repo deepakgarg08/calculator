9e0d29efc5da0b57dd6039201ac3428e973447aa
Mystic Research <mysticresearcher@gmail.com>
2023-03-10 16:05:24 +0100

    multiplication done

--- calculator.js
+++ calculator.js
@@ -1,10 +1,9 @@
 const add = require('./add');
 const sub = require('./sub');
+const mul = require('./mul');
 console.log('add (10,2): ', add (10,2));
 console.log('sub (10,2): ', sub (10,2));
-
-
-
+console.log('mul (10,2): ', mul (10,2));
 
 
 
