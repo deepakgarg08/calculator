77f0dc740e5f1fe44c7f105dc0792d92ac92a808
Mystic Research <mysticresearcher@gmail.com>
2023-03-10 16:13:13 +0100

    divide done

--- calculator.js
+++ calculator.js
@@ -1,9 +1,11 @@
 const add = require('./add');
 const sub = require('./sub');
 const mul = require('./mul');
+const divide = require('./divide');
 console.log('add (10,2): ', add (10,2));
 console.log('sub (10,2): ', sub (10,2));
 console.log('mul (10,2): ', mul (10,2));
+console.log('divide (10,2): ', divide (10,2));
 
 
 
