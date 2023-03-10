3253c28d8f53c844a40cce147fddf199517bc3c4
Mystic Research <mysticresearcher@gmail.com>
2023-03-10 15:15:01 +0100

    subtration added and code restructured

--- calculator.js
+++ calculator.js
@@ -1,8 +1,8 @@
-function add(a, b) {
-  return a + b;
-}
-let result = add (10,2)
-console.log('result: ', result);
+const add = require('./add');
+const sub = require('./sub');
+console.log('add (10,2): ', add (10,2));
+console.log('sub (10,2): ', sub (10,2));
+
 
 
 
