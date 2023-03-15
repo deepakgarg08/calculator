893469e532613c5cb33290422b48c8f3d22e3d5f
Mystic Research <mysticresearcher@gmail.com>
2023-03-15 14:39:06 +0100

    sine 90 added

--- calculator.js
+++ calculator.js
@@ -4,6 +4,6 @@ const mul = require('./mul');
 console.log('add (10,2): ', add (10,2));
 console.log('sub (10,2): ', sub (10,2));
 console.log('mul (10,2): ', mul (10,2));
-
+console.log("sin(90)", 1)
 
 
